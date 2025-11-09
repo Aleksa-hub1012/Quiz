8b53a098f772051df0c8cd9360ce4e517feb7603
Aleksa <vitusinskasasa@gmail.com>
2025-11-09 18:50:49 +0200

    Ініціалізація тесту (гра обнуляється після перезапуску)

--- QuizTests/QuizGameTests.cs
+++ QuizTests/QuizGameTests.cs
@@ -78,5 +78,20 @@ namespace Quiz.Tests
             var game = new QuizGame();
             Assert.Equal(0.0, game.CorrectAnswerPercentage);
         }
+        [Fact]
+        public void Restart_WhenCalled_ShouldResetAllStats()
+        {
+            var game = new QuizGame();
+            game.GiveAnswer(true, 10);
+            game.GiveAnswer(false, 15);
+            game.GiveAnswer(true, 5);
+
+            game.Restart();
+
+            Assert.Equal(0, game.Score);
+            Assert.Equal(0, game.CorrectAnswers);
+            Assert.Equal(0, game.TotalQuestionsAnswered);
+            Assert.Equal(0.0, game.CorrectAnswerPercentage);
+        }
     }    
 }
