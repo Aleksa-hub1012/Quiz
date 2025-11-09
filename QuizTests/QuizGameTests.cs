19d7710f61b8466fe8dfc4c95673fcf3d899bd3b
Aleksa <vitusinskasasa@gmail.com>
2025-11-09 18:10:31 +0200

    Ініціалізація тесту (перевірка початкового стану об'єкту гри)

--- QuizTests/QuizGameTests.cs
+++ QuizTests/QuizGameTests.cs
@@ -5,6 +5,15 @@ namespace Quiz.Tests
 {
     public class QuizGameTests
     {
-      
+        [Fact]
+        public void Constructor_WhenGameStarts_ShouldInitializeWithZeroStats()
+        {
+            var game = new QuizGame();
+
+            Assert.Equal(0, game.Score);
+            Assert.Equal(0, game.CorrectAnswers);
+            Assert.Equal(0, game.TotalQuestionsAnswered);
+            Assert.Equal(0.0, game.CorrectAnswerPercentage);
+        }
     }    
 }
